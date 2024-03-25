3cfdcd53ab8a76d5074c88b518b252a11d3b6c78
BeautyVika <vikachk54@gmail.com>
2024-03-25 15:11:51 +0300

    add exception

---
+++ src/main/java/com/spring/rest/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,12 @@
+package com.spring.rest.exception_handling;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmployeeIncorrectData {
+    private String info;
+}
